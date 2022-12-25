c075851529b4516a676e5052b78cb2675e325358
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-25 11:29:33 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,15 @@ input.onButtonPressed(Button.A, function () {
     vstup += 1
     basic.showString("" + (vstup))
 })
+input.onPinPressed(TouchPin.P2, function () {
+    if (scitani == 0) {
+        basic.showString("ODCITANI")
+        scitani = 1
+    } else {
+        basic.showString("SCITANI")
+        scitani = 0
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     pocitani()
 })
@@ -9,18 +18,31 @@ input.onButtonPressed(Button.B, function () {
     if (scitani == 0) {
         if (cislo1 + cislo2 == vstup) {
             basic.showIcon(IconNames.Yes)
+            game.addScore(1)
             nastav()
             basic.pause(500)
             pocitani()
         } else {
             basic.showIcon(IconNames.No)
+            game.removeLife(1)
             nastav()
             basic.pause(500)
             pocitani()
         }
     } else {
-        if (true) {
-        	
+        if (cislo_od - cislo_1_od == vstup) {
+            basic.showIcon(IconNames.Yes)
+            game.addScore(1)
+            nastav()
+            basic.pause(500)
+            pocitani()
+        } else {
+            basic.showIcon(IconNames.No)
+            game.removeLife(1)
+            basic.showNumber(cislo_od - cislo_1_od)
+            nastav()
+            basic.pause(500)
+            pocitani()
         }
     }
 })
@@ -30,7 +52,9 @@ function nastav () {
         cislo1 = randint(0, 5)
         cislo2 = randint(0, 5)
     } else {
-    	
+        cislo_od = randint(0, 10)
+        cislo_1_od = randint(0, cislo_od)
+        vstup = 0
     }
 }
 function pocitani () {
@@ -55,7 +79,25 @@ function pocitani () {
             . . . . .
             `)
     } else {
-    	
+        basic.showString("" + (cislo_od))
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showString("" + (cislo_1_od))
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
     }
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
@@ -67,12 +109,14 @@ let cislo2 = 0
 let cislo1 = 0
 let vstup = 0
 let scitani = 0
+game.setLife(3)
+game.setScore(0)
 scitani = 0
 vstup = 0
 if (scitani == 0) {
     cislo1 = randint(0, 5)
     cislo2 = randint(0, 5)
 } else {
-    cislo_1_od = 0
-    cislo_od = 0
+    cislo_1_od = randint(0, 10)
+    cislo_od = randint(0, cislo_od)
 }
