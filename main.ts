dc9184af7d7b7a9f1727b2fd979d7cdc443a289b
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-26 16:43:26 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,7 +25,7 @@ input.onButtonPressed(Button.B, function () {
         } else {
             basic.showIcon(IconNames.No)
             game.addScore(-1)
-            game.removeLife(1)
+            game.removeLife(-1)
             nastav()
             basic.pause(500)
             pocitani()
@@ -40,7 +40,7 @@ input.onButtonPressed(Button.B, function () {
         } else {
             basic.showIcon(IconNames.No)
             game.addScore(-1)
-            game.removeLife(1)
+            game.removeLife(-1)
             basic.showNumber(cislo_od - cislo_1_od)
             nastav()
             basic.pause(500)
